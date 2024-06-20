73e9bbde8e767a8d99b010f3d5c8aa98d76a2c09
SirWerto <sirwerto@gmail.com>
2024-06-20 11:27:10 +0200

    force compile for rpi4

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule TLake.MixProject do
       {:travianmap, "1.0.0"},
       {:gen_stage, "~> 1.2"},
       {:nx, "0.7.2"},
-      {:exgboost, "0.5.0"},
+      {:exgboost, "0.5.0", compile: "make"},
       {:explorer, "0.8.2", system_env: %{"EXPLORER_BUILD" => "1"}}
     ]
   end
